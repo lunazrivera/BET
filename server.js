7db27e2594c44871cc055762baaa78868737328e
arinmsn <arinmsn@gmail.com>
2019-05-07 15:08:26 -0700

    Able to successfully register & login

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const path = require("path");
 const mongoose = require("mongoose");
 const passport = require("passport");
 
+// Go to user route
 const users = require("./routes/api/user")
 
 const PORT = process.env.PORT || 3002;
